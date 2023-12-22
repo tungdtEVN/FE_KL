23565035198e55380f33992d25a5c684c0abb023
tungdtEVN <117056329+tungdtEVN@users.noreply.github.com>
2023-12-22 13:25:40 +0700

    fix lỗi home blog

--- src/component/HomeFeature/HomeBlogItem.js
+++ src/component/HomeFeature/HomeBlogItem.js
@@ -10,15 +10,15 @@ function HomeBlogItem(props) {
                 </div>
                 <div className="short_details">
                     <div className="meta-top d-flex">
-                        <a >{props.data.userData.firstName +" "+props.data.userData.lastName}</a>
-                        <a ><i className="ti-comments-smiley" />{props.data.commentData.length} Bình luận</a>
+                        <a >{props?.data?.userData?.firstName +" "+props?.data?.userData?.lastName}</a>
+                        <a ><i className="ti-comments-smiley" />{props?.data?.commentData?.length} Bình luận</a>
                     </div>
-                    <Link className="d-block" to={`/blog-detail/${props.data.id}`}>
-                        <h4>{props.data.title}</h4>
+                    <Link className="d-block" to={`/blog-detail/${props?.data?.id}`}>
+                        <h4>{props?.data?.title}</h4>
                     </Link>
                     <div className="text-wrap">
                         <p>
-                            {props.data.description}
+                            {props?.data?.description}
                         </p>
                     </div>
                     <a  className="blog_btn">Xem thêm<span className="ml-2 ti-arrow-right" /></a>
